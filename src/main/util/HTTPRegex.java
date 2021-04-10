b2d5486656e7a17bf51382edda5990979c5342d5
Bryan Sheranian <bsheranian1@gmail.com>
2021-04-10 17:24:20 -0600

    add AWS API Gatway Swagger file for all 22 fully functioning endpoints

--- src/main/util/HTTPRegex.java
+++ src/main/util/HTTPRegex.java
@@ -5,7 +5,7 @@ public class HTTPRegex {
   public static String INCORRECT_USERNAME = "[Incorrect Username]";  //400
   public static String INCORRECT_PASSWORD = "[Incorrect Password]";  //410
   public static String SESSION_EXPIRED = "[Session Expired]";  //420
-  public static String EMAIL_TAKEN = "[Email Already Associated With User]"; //430
+  public static String EMAIL_TAKEN = "[Username Already Taken]"; //430
   public static String SERVER_ERROR = "[Server Error]";  //500
 }
 
